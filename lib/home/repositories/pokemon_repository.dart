706c1b3485e4c41926ce10ac5faac0534bcb942a
Jose Rodriguez <j.rodriguez@tvup.media>
2024-05-06 12:01:32 +0200

    feat: obtain pokemon list according to type endpoint

--- lib/home/repositories/pokemon_repository.dart
+++ lib/home/repositories/pokemon_repository.dart
@@ -1,5 +1,8 @@
 import '../domain/pokemon_detail_item.dart';
+import '../domain/pokemon_type.dart';
 
 abstract class PokemonRepository {
   Future<List<PokemonDetailItem>> fetch();
+  Future<List<PokemonTypeItem>> fetchTypes();
+  Future<List<PokemonDetailItem>> fetchPokemonListByType(String url);
 }
--- lib/home/repositories/pokemon_respository_impl.dart
+++ lib/home/repositories/pokemon_respository_impl.dart
@@ -4,6 +4,7 @@ import 'package:pokedex/core/config/config.dart';
 import 'package:pokedex/home/domain/pokemon_detail_item.dart';
 import 'package:pokedex/home/domain/pokemon_item.dart';
 import 'package:http/http.dart' as http;
+import 'package:pokedex/home/domain/pokemon_type.dart';
 
 import 'pokemon_repository.dart';
 
@@ -24,6 +25,52 @@ class PokemonRepositoryImpl extends PokemonRepository {
         if (response.statusCode == 200) {
           final result = jsonDecode(responseItem.body);
           pokemonDetailList.add(PokemonDetailItem.fromJson(result));
+        } else {
+          throw Exception();
+        }
+      }
+      return pokemonDetailList;
+    } else {
+      throw Exception();
+    }
+  }
+
+  @override
+  Future<List<PokemonTypeItem>> fetchTypes() async {
+    List<PokemonTypeItem> pokemonItemList = [];
+    var url = Uri.parse(Config.typesUrl);
+    var response = await http.get(url);
+    if (response.statusCode == 200) {
+      final results = jsonDecode(response.body)['results'];
+      pokemonItemList = results
+          .map<PokemonTypeItem>((value) => PokemonTypeItem.fromJson(value))
+          .toList();
+      return pokemonItemList;
+    } else {
+      throw Exception();
+    }
+  }
+
+  @override
+  Future<List<PokemonDetailItem>> fetchPokemonListByType(String url) async {
+    var uri = Uri.parse(url);
+    var response = await http.get(uri);
+    if (response.statusCode == 200) {
+      final results = jsonDecode(response.body)['pokemon'];
+      final List<PokemonItem> pokemonItemList = results
+          .map<PokemonItem>((value) => PokemonItem.fromJson(value['pokemon']))
+          .toList();
+      List<PokemonDetailItem> pokemonDetailList = [];
+      for (var pokemonItem in pokemonItemList) {
+        var urlDetail = Uri.parse(pokemonItem.url);
+        var responseItem = await http.get(urlDetail);
+        if (response.statusCode == 200) {
+          try {
+            final result = jsonDecode(responseItem.body);
+            pokemonDetailList.add(PokemonDetailItem.fromJson(result));
+          } on Exception catch (e) {
+            print(e);
+          }
         } else {
           throw Exception();
         }
